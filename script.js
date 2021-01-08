14b03804f121144165d2ab3bcd7a3884deeeba0c
wgjedste <wgjedste@gmail.com>
2021-01-08 18:57:13 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(item1, item2){
+  return item1 + item2
+}
+
+function subtract(item1, item2){
+  return item1 - item2
+}
+
+function multiply(item1, item2){
+  return item1 * item2
+}
+
+function divide(item1, item2){
+  return item1 / item2
+}
+
+function percentage(item1, item2){
+  return item1 / 100
+}
